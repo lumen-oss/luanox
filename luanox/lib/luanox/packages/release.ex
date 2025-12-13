fd6d2dde7b4e778ef2c378c30eb1f2481a0efa1e
vhyrro <vhyrro@gmail.com>
2025-12-13 19:10:37 +0100

    feat: parse versions as true semver internally (with sorting in the UI)

--- luanox/lib/luanox/packages/release.ex
+++ luanox/lib/luanox/packages/release.ex
@@ -1,11 +1,10 @@
 defmodule LuaNox.Packages.Release do
-  alias LuaNox.Packages
   use Ecto.Schema
   import Ecto.Changeset
   import Logger, only: :macros
 
   schema "releases" do
-    field :version, :string
+    field :version, LuaNox.Types.Version
     field :rockspec_path, :string
     field :rockspec, :any, virtual: true
     field :download_count, :integer, default: 0
@@ -20,16 +19,29 @@ defmodule LuaNox.Packages.Release do
     |> cast(attrs, [:version, :rockspec])
     |> validate_required([:version, :rockspec])
     |> unique_constraint([:version, :package_id], name: :unique_package_version)
-    |> validate_format(:version, ~r/^\d+(\.\d+){2}\-\d+$/)
+    |> validate_version_format()
     |> validate_rockspec(attrs["package"])
   end
+
+  defp validate_version_format(changeset) do
+    validate_change(changeset, :version, fn :version, version ->
+      if valid_format?(version) do
+        []
+      else
+        [version: "must be in format X.Y.Z-W"]
+      end
+    end)
+  end
+
+  defp valid_format?(%Version{pre: [pre]}) when is_integer(pre), do: true
+  defp valid_format?(_), do: false
 
   defp validate_rockspec(changeset, package) do
     rockspec = get_field(changeset, :rockspec)
     version = get_field(changeset, :version)
 
     case Application.get_env(:luanox, :rockspec_verification_endpoint)
-         |> Req.post(json: %{rockspec: rockspec, package: package, version: version}) do
+         |> Req.post(json: %{rockspec: rockspec, package: package, version: to_string(version)}) do
       {:ok, %Req.Response{status: 200}} ->
         changeset
         |> put_change(:rockspec_path, "#{package}-#{version}.rockspec")
