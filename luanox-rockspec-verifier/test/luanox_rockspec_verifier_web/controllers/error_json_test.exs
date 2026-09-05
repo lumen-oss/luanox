01e225d1701c982d1c1c445f878fbfaa181e5734
NTBBloodbath <bloodbathalchemist@protonmail.com>
2026-09-04 20:16:53 -0400

    chore(luanox-rockspec-verifier): mix format

--- luanox-rockspec-verifier/test/luanox_rockspec_verifier_web/controllers/error_json_test.exs
+++ luanox-rockspec-verifier/test/luanox_rockspec_verifier_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule LuanoxRockspecVerifierWeb.ErrorJSONTest do
   use LuanoxRockspecVerifierWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert LuanoxRockspecVerifierWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert LuanoxRockspecVerifierWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
